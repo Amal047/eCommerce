aa95bde8d466ab1f830df9935b57fca47fbb7c67
Amal047 <amal.bvp@gmail.com>
2025-03-18 23:46:36 +0530

    unwanted images

--- Ecom/Ecom/settings.py
+++ Ecom/Ecom/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'Ecom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Template')],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
